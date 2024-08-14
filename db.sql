dda711f31a862cce5d3b92eb08938f35a04ccaaf
Anlimah <y.m.ratty7@gmail.com>
2024-08-14 19:54:08 +0000

    Users can generated code for payment

--- db.sql
+++ db.sql
@@ -17,3 +17,15 @@ CREATE TABLE `foreign_form_purchase_requests` (
     CONSTRAINT `fk_ffpr_form` FOREIGN KEY (`form`) REFERENCES `forms` (`id`) ON UPDATE CASCADE ON DELETE SET NULL,
     CONSTRAINT `fk_ffpr_admission_period` FOREIGN KEY (`admission_period`) REFERENCES `admission_period` (`id`) ON UPDATE CASCADE ON DELETE SET NULL
 );
+
+CREATE INDEX idx_reference_number ON `foreign_form_purchase_requests` (`reference_number`);
+CREATE INDEX idx_first_name ON `foreign_form_purchase_requests` (`first_name`);
+CREATE INDEX idx_last_name ON `foreign_form_purchase_requests` (`last_name`);
+CREATE INDEX idx_email_address ON `foreign_form_purchase_requests` (`email_address`);
+CREATE INDEX idx_p_country_name ON `foreign_form_purchase_requests` (`p_country_name`);
+CREATE INDEX idx_p_country_code ON `foreign_form_purchase_requests` (`p_country_code`);
+CREATE INDEX idx_phone_number ON `foreign_form_purchase_requests` (`phone_number`);
+CREATE INDEX idx_s_country_name ON `foreign_form_purchase_requests` (`s_country_name`);
+CREATE INDEX idx_s_country_code ON `foreign_form_purchase_requests` (`s_country_code`);
+CREATE INDEX idx_support_number ON `foreign_form_purchase_requests` (`support_number`);
+CREATE INDEX idx_added_at ON `foreign_form_purchase_requests` (`added_at`);
